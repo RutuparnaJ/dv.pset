4a73e776beeabcc8de27d423bd8b8af32a83505e
RutuparnaJ <RutuparnaJ@users.noreply.github.com>
2018-08-26 23:05:37 +0530

    Linear Search

---
+++ LinearSearch.py
@@ -0,0 +1,11 @@
+#Linear Search
+
+arr = [1, 2, 5, 7, 10, 21, 27, 30, 41]
+
+
+search= input(int("Enter the number to be searched: "))
+for i in range (0,10):
+    if(search==arr(i)):
+        print("Number found!")
+    else:
+        print("Number not found")
